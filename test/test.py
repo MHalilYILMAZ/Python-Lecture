29802c6d3e0cd77f486f4401353a526b0d14f1a0
muhammedhalilyilmaz <muhammedhyilmaz54@gmail.com>
2023-02-28 23:13:38 +0300

    first test

--- test/test.py
+++ test/test.py
@@ -1,3 +1,13 @@
-print('Hello world')
-print('Emrah')
-print('halil')
+print("Oyuncu Kaydetme Programı")
+
+ad = input("Oyuncunun Adı:")
+soyad = input("Oyuncunun Soyadı:")
+takım = input("Oyuncunun Takımı:")
+
+bilgiler = [ad, soyad, takım]
+
+print("Bilgiler kaydediliyor....")
+
+print(" Oyuncu Adı: {}\n Oyuncu Soyadı: {}\n Oyuncunun Takımı: {}\n ".format(bilgiler[0], bilgiler[1], bilgiler[2]))
+
+print("Bilgiler Kaydedildi.")
